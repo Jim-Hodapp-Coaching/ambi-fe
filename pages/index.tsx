895c10ca799ebc4cccaf5604db9881531b5b91ad
David A <david@herokode.com>
2023-03-09 21:51:01 -0500

    Add Container & Grid Skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Card, Container, Grid } from '@mui/material';
 
 import styles from '@/pages/index.module.css'
 
@@ -15,62 +12,24 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Container>
+          <h1 className={styles.title}>
+            Welcome to Next.js!
+          </h1>
+          <Grid container spacing={2}>
+            <Grid item xs={6} sm={4}>
+              <Card variant='outlined'>Temperature</Card>
+            </Grid>
+            <Grid item xs={6} sm={4}>
+              <Card variant='outlined'>Temperature</Card>
+            </Grid>
+            <Grid item xs={6} sm={4}>
+              <Card variant='outlined'>Temperature</Card>
+            </Grid>
+          </Grid>
+        </Container>
       </main>
 
-      <Container maxWidth="sm">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Material UI Create React App example with styled-components
-          </Typography>
-        </Box>
-   </Container>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
